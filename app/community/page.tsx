c6146a89dfcee99e180bd7fdcace4d55e883b994
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2024-08-18 17:48:44 -0300

    Correct resolution
    The answer provided by me, on previews commit, was adjusted. In accordance to the answer of the instructor. O mainly forgot about the community page.

---
+++ app/community/page.tsx
@@ -0,0 +1,9 @@
+export default function Community(): JSX.Element {
+    return (
+        <main>
+            <h1 style={{ color: 'white', textAlign: 'center' }}>
+                Community
+            </h1>
+        </main>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home(): JSX.Element {
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
-      <Link href="/meals">View meals</Link>
+      <p><Link href="/meals">View meals</Link></p>
+      <p><Link href="/meals/share">Share recipes</Link></p>
+      <p><Link href="/community">View community</Link></p>
     </main>
   );
 }
