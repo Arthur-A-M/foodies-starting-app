13308f09654456292a2b04589b41708f03e6fae8
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2024-08-19 18:26:19 -0300

    Create Background
    Background is a MainHeader sub folder now. Inside the components folder.

--- components/MainHeader/index.tsx
+++ components/MainHeader/index.tsx
@@ -3,21 +3,26 @@ import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 
+import Background from "./Background";
+
 import classes from './main-header.module.css';
 
 export default function MainHeader(): JSX.Element {
     return (
-        <header className={classes.header}>
-            <Link href="/" className={classes.logo}>
-                <Image src={logoImg} alt="Logo" priority />
-                NextLevel Food
-            </Link>
-            <nav className={classes.nav}>
-                <ul>
-                    <li><Link href="/meals">Browse Meals</Link></li>
-                    <li><Link href="/community">Foodies Community</Link></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <Background />
+            <header className={classes.header}>
+                <Link href="/" className={classes.logo}>
+                    <Image src={logoImg} alt="Logo" priority />
+                    NextLevel Food
+                </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li><Link href="/meals">Browse Meals</Link></li>
+                        <li><Link href="/community">Foodies Community</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 }
