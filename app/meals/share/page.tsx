c89f7ec954b6f7e6b6ef68d98652289af2633410
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2024-09-07 20:29:24 -0300

    Create server function MealType
    MealType was created but it only receives the data and console log it, rather than update the data base.

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,8 +1,26 @@
 import classes from './page.module.css';
 
 import { ImagePicker } from '@/components';
+//import MealType from '@/app/meals/[slug]';
 
 export default function Share(): JSX.Element {
+  async function shareMeal(formData: FormData)/*: Promise<Response>*/ {
+    'use server';
+    
+    const meal/*: MealType*/ = {
+      //id: formData.get('id') as unknown as number,
+      //slug: formData.get('slug') as string,
+      title: formData.get('title') as string,
+      image: formData.get('image') as string,
+      summary: formData.get('summary') as string,
+      instructions: formData.get('instructions') as string,
+      creator: formData.get('name') as string,
+      creator_email: formData.get('email') as string
+    };
+
+    return console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +30,7 @@ export default function Share(): JSX.Element {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,7 +58,7 @@ export default function Share(): JSX.Element {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label='Your image' name="image"  />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
