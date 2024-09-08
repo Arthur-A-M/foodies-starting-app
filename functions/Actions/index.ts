90854561716c8e98ab646c51545ca8961452ca88
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2024-09-08 08:15:27 -0300

    Add server side user input handling

--- functions/Actions/index.ts
+++ functions/Actions/index.ts
@@ -5,18 +5,34 @@ import { redirect } from 'next/navigation';
 
 //import type { MealType } from '@/types';
 
+function isValidText(text: string) {
+  return (!text || text.trim() !== '');
+}
+
 export async function shareMeal(formData: FormData)/*: Promise<Response>*/ {
   const meal/*: MealType*/ = {
     //id: formData.get('id') as unknown as number,
     //slug: formData.get('slug') as string,
     title: formData.get('title') as string,
-    image: formData.get('image') as string,
+    image: formData.get('image'),
     summary: formData.get('summary') as string,
     instructions: formData.get('instructions') as string,
     creator: formData.get('name') as string,
     creator_email: formData.get('email') as string
   };
 
+  if (
+    !isValidText(meal.title) ||
+    !isValidText(meal.summary) ||
+    !isValidText(meal.instructions) ||
+    !isValidText(meal.creator) ||
+    !isValidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image || meal.image.size === 0
+  ) {
+    throw new Error('Invalid input');
+  }
+
   await saveMeal(meal);
-  redirect('/meals');
+  redirect('/meals'); // should go within try catch block
 }
