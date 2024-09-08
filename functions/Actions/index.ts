08b52d09b10fa120a2b616c49f3f8024191fc7da
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2024-09-08 07:39:44 -0300

    Redirect the user after adding an image
    After an image is added to the database the user is redirected to /meals.

--- functions/Actions/index.ts
+++ functions/Actions/index.ts
@@ -1,23 +1,22 @@
 'use server';
 
-import { NextApiRequest, NextApiResponse } from 'next';
 import { saveMeal } from '@/functions';
-import { redirect } from 'next/dist/server/api-utils';
+import { redirect } from 'next/navigation';
 
 //import type { MealType } from '@/types';
 
-export async function shareMeal(formData: FormData, req: NextApiRequest, res: NextApiResponse)/*: Promise<Response>*/ {
-    const meal/*: MealType*/ = {
-      //id: formData.get('id') as unknown as number,
-      //slug: formData.get('slug') as string,
-      title: formData.get('title') as string,
-      image: formData.get('image') as string,
-      summary: formData.get('summary') as string,
-      instructions: formData.get('instructions') as string,
-      creator: formData.get('name') as string,
-      creator_email: formData.get('email') as string
-    };
+export async function shareMeal(formData: FormData)/*: Promise<Response>*/ {
+  const meal/*: MealType*/ = {
+    //id: formData.get('id') as unknown as number,
+    //slug: formData.get('slug') as string,
+    title: formData.get('title') as string,
+    image: formData.get('image') as string,
+    summary: formData.get('summary') as string,
+    instructions: formData.get('instructions') as string,
+    creator: formData.get('name') as string,
+    creator_email: formData.get('email') as string
+  };
 
-    await saveMeal(meal);
-    //redirect(res, 307 ,'/meals');
-  }
+  await saveMeal(meal);
+  redirect('/meals');
+}
